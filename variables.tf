1fbfd048f3033e07b3eae3af8be0c1e062033426
yebinnn <926167912@gmail.com>
2023-02-10 00:45:06 +0900

    terraform 변수 생성

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    default = 8080
+}
+
+variable "vpc-name" {
+    type = string
+    description = "The port the server will use for HTTP requests"
+    default = "tutorial_vpc"
+}
